da6f8da5e88d0d44c67e5314d64444a42647525f
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-02-24 12:43:05 +0400

    NavigasyonBetweenThorAndKratos

--- app/src/main/java/com/yusufov/myexampleapp/BottomNavActivity.kt
+++ app/src/main/java/com/yusufov/myexampleapp/BottomNavActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomNavActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,21 @@ class BottomNavActivity : AppCompatActivity() {
         var navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
-
+        val bottomView : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.action_homeMenu -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, KratosFragment()).commit()
+                    true
+                }
+                R.id.action_profile->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, ThorFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 }
