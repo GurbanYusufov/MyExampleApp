43ad0dd6001fa6a64c533a6bfeff002da57d5368
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-03-01 17:44:47 +0400

    ButtonColorForTHor

--- app/src/main/java/com/yusufov/myexampleapp/ThorFragment.kt
+++ app/src/main/java/com/yusufov/myexampleapp/ThorFragment.kt
@@ -1,5 +1,7 @@
 package com.yusufov.myexampleapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
@@ -7,13 +9,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
+import android.widget.Button
+import com.yusufov.myexampleapp.databinding.FragmentThorBinding
 
 class ThorFragment : Fragment() {
+  
 
+    private lateinit var binding: FragmentThorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val inflater = TransitionInflater.from(requireContext())
         enterTransition = inflater.inflateTransition(android.R.transition.fade)
+
 
     }
     override fun onCreateView(
@@ -21,7 +28,31 @@ class ThorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thor, container, false)
+
+        binding = FragmentThorBinding.inflate(inflater)
+
+        binding.killButton.setOnClickListener {
+            binding.ThorText.text = "Thor Geberdi Beybi"
+
+        }
+
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(savedInstanceState!=null){
+            val user = savedInstanceState.getString("thor_state")
+            binding.ThorText.text = user
+            println("AAAAAAAAAAAAAAAAAAA")
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("thor_state",binding.ThorText.text.toString())
+        println("callMeBaybe")
+    }
+
+
 
 }
